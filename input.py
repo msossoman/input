2f6bd4a6447256383a086f24589546ba722a91f0
Mark Sossoman <msossoman@gmail.com>
2015-03-26 22:36:40 -0400

    set up input repo

---
+++ input.py
@@ -0,0 +1,6 @@
+age = int(raw_input("What is your age?>"))
+name = raw_input("What is your name?>")
+tenYears = age + 10
+
+print "Hi {}, you are {}.".format(name, age)
+print "In 10 years, you'll be {}.".format(tenYears)
